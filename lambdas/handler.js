3df9991795c0470521d153814409bf2897eebc2a
Mario Yepes <marioy47@gmail.com>
2020-04-12 16:56:18 -0500

    Adding API endpoint and S3 local

--- lambdas/handler.js
+++ lambdas/handler.js
@@ -1,18 +1,16 @@
-'use strict';
+// lambdas/handler.js
+"use strict";
 
-module.exports.hello = async event => {
+module.exports.hello = async (event) => {
+  const name = event.pathParameters.name;
   return {
     statusCode: 200,
     body: JSON.stringify(
       {
-        message: 'Go Serverless v1.0! Your function executed successfully!',
-        input: event,
+        message: `Hola ${name}... It works !!!`,
       },
       null,
       2
     ),
   };
-
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
 };
